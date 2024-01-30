b4ecd6753c5cf39ca14e9891574f43e1b8d100c8
simeon <kingthesim@abv.bg>
2024-01-30 23:02:43 +0200

    project_reset

--- account/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
-from .forms import UserRegistrationForm, StaffUserRegistrationForm, SuperUserRegistrationForm
-
-
--- post/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import post_list, update_post, create_post, delete_post
-
-urlpatterns = [
-    path('posts/', post_list, name='post_list'),
-    path('update_post/<int:post_id>/', update_post, name="update_post"),
-    path('create_post/', create_post, name="create_post"),
-    path('delete_post/<int:post_id>/', delete_post, name="delete_post")
-]
